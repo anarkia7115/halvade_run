e6a65a988befc29003a0e6bd740da415a38172cf
Mark <marc7115@gmail.com>
2015-10-22 10:58:15 +0800

    add lib and ignore file

--- scripts/download.sh
+++ scripts/download.sh
@@ -1,1 +1,2 @@
-aws s3 cp s3://gcbibucket/halvade/halvade-1.0-SNAPSHOT-jar-with-dependencies.jar ./HalvadeWithLibs.jar
+aws s3 cp s3://gcbibucket/halvade/halvade-1.0-SNAPSHOT-jar-with-dependencies.jar ../lib/HalvadeWithLibs.jar
+aws s3 cp s3://gcbibucket/halvade/HalvadeUploaderWithLibs.jar ../lib/HalvadeUploaderWithLibs.jar
