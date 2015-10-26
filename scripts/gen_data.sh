8ef4b2d80dbb1a7c41f7a7910096fd9bce519f00
Mark <marc7115@gmail.com>
2015-10-26 11:06:31 +0800

    new

--- scripts/gen_data.sh
+++ scripts/gen_data.sh
@@ -1,1 +1,1 @@
-hadoop jar ./HalvadeUploaderWithLibs.jar -i -1 ./input.manifest -O hdfs:///halvade/upload_output -t 2
+java -jar ../lib/HalvadeUploaderWithLibs.jar -i -1 ./input.manifest -O hdfs:///halvade/upload_output -t 2
